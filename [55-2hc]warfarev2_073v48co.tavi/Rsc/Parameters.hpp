1684fcdeda70f46df953e276789a573347631f7f
Miksuu <imiksuu96@gmail.com>
2020-01-29 19:53:24 +0200

    Finishing the camp, depot, defences positions, disable ICBM by default

--- [55-2hc]warfarev2_073v48co.tavi/Rsc/Parameters.hpp
+++ [55-2hc]warfarev2_073v48co.tavi/Rsc/Parameters.hpp
@@ -334,7 +334,7 @@ class Params {
 		title = "$STR_WF_PARAMETER_ICBM";
 		values[] = {0,1};
 		texts[] = {"$STR_WF_Disabled","$STR_WF_Enabled"};
-		default = 1;
+		default = 0;
 	};
 	class WFBE_C_MODULE_WFBE_IRS {
 		title = "$STR_WF_PARAMETER_IRS";
