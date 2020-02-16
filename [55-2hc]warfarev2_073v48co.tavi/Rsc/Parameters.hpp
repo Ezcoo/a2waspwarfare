c8389aee600ee6f49144e3c36acd81c20d4b5d63
Miksuu <imiksuu96@gmail.com>
2020-02-16 23:27:54 +0200

    Change starting month to November, time to 10:00

--- [55-2hc]warfarev2_073v48co.tavi/Rsc/Parameters.hpp
+++ [55-2hc]warfarev2_073v48co.tavi/Rsc/Parameters.hpp
@@ -158,13 +158,13 @@ class Params {
 		values[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
 		texts[] = {"00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00",
 		"12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"};
-		default = 9;
+		default = 10;
 	};
 	class WFBE_C_ENVIRONMENT_STARTING_MONTH {
 		title = "$STR_WF_PARAMETER_Month";
 		values[] = {1,2,3,4,5,6,7,8,9,10,11,12};
 		texts[] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
-		default = 6;
+		default = 11;
 	};
 	class WFBE_C_ENVIRONMENT_WEATHER_VOLUMETRIC {
 		title = "$STR_WF_PARAMETER_Clouds";
