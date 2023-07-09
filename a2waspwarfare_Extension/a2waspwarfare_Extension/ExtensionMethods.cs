4c6f4c3cf06061f23957abe786c3cc2b1b2c95b2
Miksuu <imiksuu96@gmail.com>
2023-07-10 00:54:48 +0300

    Remove some GPT comments, fix syntax

--- a2waspwarfare_Extension/a2waspwarfare_Extension/ExtensionMethods.cs
+++ a2waspwarfare_Extension/a2waspwarfare_Extension/ExtensionMethods.cs
@@ -20,7 +20,6 @@ namespace a2waspwarfare_Extension
     public abstract class BaseExtensionClass : InterfaceExtension
     {
         public abstract void ActivateExtensionMethod(string[] _args);
-        // Add other abstract methods or properties here if needed
     }
 
     public class TestExtensionClass : BaseExtensionClass
@@ -29,74 +28,72 @@ namespace a2waspwarfare_Extension
         {
             Log.WriteLine("testing functionality of " + nameof(TestExtensionClass));
             Log.WriteLine("asd", LogLevel.DEBUG);
-            // Implement specific functionality for TestExtensionClass here
-            // Use _args parameter as needed
         }
 
-        // Add any additional methods or properties specific to TestExtensionClass
-    }
-
-    public class ActualExtensionClass : BaseExtensionClass
-    {
-        public override void ActivateExtensionMethod(string[] _args)
+        public class ActualExtensionClass : BaseExtensionClass
         {
-            Log.WriteLine("testing functionality of " + nameof(ActualExtensionClass));
-        }
-    }
-
-
-    public class ExtensionMethods
-    {
-        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
-        public static void RvExtension(StringBuilder _output, int _outputSize, [MarshalAs(UnmanagedType.LPStr)] string _argsAsString)
-        {
-            Log.WriteLine("Received args as: " + _argsAsString);
-
-            var splitArgsArray = SplitArgsToArray(_argsAsString);
-            var extensionNameAsString = splitArgsArray[0];
-            splitArgsArray = RemoveFirstElement(splitArgsArray);
-
-            Log.WriteLine("Reveived args count: " + splitArgsArray.Length + " with " + nameof(BaseExtensionClass));
-            foreach (var item in splitArgsArray)
+            public override void ActivateExtensionMethod(string[] _args)
             {
-                Log.WriteLine(item);
-            }
-
-            if (!Enum.TryParse(extensionNameAsString, out ExtensionName _extensionName))
-            {
-                Log.WriteLine("Invalid extension name: " + extensionNameAsString, LogLevel.CRITICAL);
-                return;
-            }
-
-            BaseExtensionClass extensionClass = (BaseExtensionClass)GetExtensionInstance(_extensionName);
-            extensionClass.ActivateExtensionMethod(splitArgsArray);
-        }
-
-        private static InterfaceExtension GetExtensionInstance(ExtensionName _extensionName)
-        {
-            Log.WriteLine("Getting instance: " + _extensionName, LogLevel.DEBUG);
-            try
-            {
-                return (InterfaceExtension)EnumExtensions.GetInstance(_extensionName.ToString());
-            }
-            catch (Exception ex)
-            {
-                Log.WriteLine(ex.Message, LogLevel.CRITICAL);
-                throw new InvalidOperationException(ex.Message);
+                Log.WriteLine("testing functionality of " + nameof(ActualExtensionClass));
             }
         }
 
-        public static string[] SplitArgsToArray(string _argsString)
-        {
-            Log.WriteLine("Splitting args to array: " + _argsString);
-            return _argsString.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-        }
 
-        public static string[] RemoveFirstElement(string[] _array)
+        public class ExtensionMethods
         {
-            string[] newArray = new string[_array.Length - 1];
-            Array.Copy(_array, 1, newArray, 0, _array.Length - 1);
-            return newArray;
+            [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+            public static void RvExtension(StringBuilder _output, int _outputSize, [MarshalAs(UnmanagedType.LPStr)] string _argsAsString)
+            {
+
+
+                Log.WriteLine("Received args as: " + _argsAsString);
+
+                var splitArgsArray = SplitArgsToArray(_argsAsString);
+                var extensionNameAsString = splitArgsArray[0];
+                splitArgsArray = RemoveFirstElement(splitArgsArray);
+
+                Log.WriteLine("Reveived args count: " + splitArgsArray.Length + " with " + nameof(BaseExtensionClass));
+                foreach (var item in splitArgsArray)
+                {
+                    Log.WriteLine(item);
+                }
+
+                if (!Enum.TryParse(extensionNameAsString, out ExtensionName _extensionName))
+                {
+                    Log.WriteLine("Invalid extension name: " + extensionNameAsString, LogLevel.CRITICAL);
+                    return;
+                }
+
+                BaseExtensionClass extensionClass = (BaseExtensionClass)GetExtensionInstance(_extensionName);
+                extensionClass.ActivateExtensionMethod(splitArgsArray);
+            }
+
+            private static InterfaceExtension GetExtensionInstance(ExtensionName _extensionName)
+            {
+                Log.WriteLine("Getting instance: " + _extensionName, LogLevel.DEBUG);
+                try
+                {
+                    return (InterfaceExtension)EnumExtensions.GetInstance(_extensionName.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+                    throw new InvalidOperationException(ex.Message);
+                }
+            }
+
+            public static string[] SplitArgsToArray(string _argsString)
+            {
+                Log.WriteLine("Splitting args to array: " + _argsString);
+                return _argsString.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            public static string[] RemoveFirstElement(string[] _array)
+            {
+                string[] newArray = new string[_array.Length - 1];
+                Array.Copy(_array, 1, newArray, 0, _array.Length - 1);
+                return newArray;
+            }
         }
     }
 }
