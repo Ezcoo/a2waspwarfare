1ab03f60638824598b0dc6d3c9e9472ecff77b41
Miksuu <imiksuu96@gmail.com>
2023-07-12 00:58:35 +0300

    Add the channel files

---
+++ a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/Interfaces/Messages/Implementations/GAMESTATUSMESSAGE.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using System.Data;
+using System;
+using System.Runtime.Serialization;
+using Discord.WebSocket;
+using System.Threading.Channels;
+using System.Reflection;
+using System.Collections.Concurrent;
+
+[DataContract]
+public class GAMESTATUSMESSAGE : BaseMessage
+{
+    public GAMESTATUSMESSAGE()
+    {
+        thisInterfaceMessage.MessageName = MessageName.GAMESTATUSMESSAGE;
+
+        thisInterfaceMessage.MessageButtonNamesWithAmount = new ConcurrentDictionary<ButtonName, int>(
+            new ConcurrentBag<KeyValuePair<ButtonName, int>>()
+            {
+            });
+
+        thisInterfaceMessage.MessageEmbedTitle = "Game Status (scores)";
+    }
+
+    protected override void GenerateButtons(ComponentBuilder _component, ulong _leagueCategoryId)
+    {
+        base.GenerateRegularButtons(_component, _leagueCategoryId);
+    }
+
+    public override Task<string> GenerateMessage(ulong _leagueCategoryId = 0)
+    {
+        string message = string.Empty;
+
+        message += "BLUFOR: " + GameData.Instance.exportedArgs[0] + "\n";
+        message += "OPFOR: " + GameData.Instance.exportedArgs[1] + "\n\n" +
+            "Please balance teams accordingly!";
+
+        return Task.FromResult(message);
+    }
+}
