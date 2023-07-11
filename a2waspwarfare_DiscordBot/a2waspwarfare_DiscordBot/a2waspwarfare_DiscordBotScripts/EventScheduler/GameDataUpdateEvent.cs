73fce10ba982fd8ba67b3fe51e668060c03329cf
Miksuu <imiksuu96@gmail.com>
2023-07-12 00:54:42 +0300

    Update gitignore, add the files again

---
+++ a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/EventScheduler/GameDataUpdateEvent.cs
@@ -0,0 +1,49 @@
+﻿using Discord.Rest;
+using System.Collections.Concurrent;
+using System.Runtime.Serialization;
+
+[DataContract]
+public class GameDataUpdateEvent : ScheduledEvent
+{
+    public GameDataUpdateEvent() { }
+
+    public GameDataUpdateEvent(
+        ConcurrentBag<ScheduledEvent> _scheduledEvents)
+    {
+        SetupScheduledEvent(133333333333337, _scheduledEvents, 60);
+
+        Log.WriteLine("Done creating event: " + nameof(GameDataUpdateEvent));
+    }
+
+    public override async Task ExecuteTheScheduledEvent(bool _serialize = true)
+    {
+        try
+        {
+            Log.WriteLine("This event should not be executed!", LogLevel.CRITICAL);
+        }
+        catch (Exception ex)
+        {
+            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+            return;
+        }
+    }
+
+    public override void CheckTheScheduledEventStatus()
+    {
+        try
+        {
+            GameDataDeSerialization.DeSerializeGameDataFromExtension();
+
+            Database.Instance.Categories.FindInterfaceCategoryByCategoryName(
+                CategoryType.GAMESTATUSCATEGORY).FindInterfaceChannelWithNameInTheCategory(
+                    ChannelType.GAMESTATUSCHANNEL).FindInterfaceMessageWithNameInTheChannel(
+                        MessageName.GAMESTATUSMESSAGE).GenerateAndModifyTheMessage();
+        }
+        catch (Exception ex)
+        {
+            Log.WriteLine(ex.Message, LogLevel.CRITICAL);
+            return;
+        }
+    }
+
+}
