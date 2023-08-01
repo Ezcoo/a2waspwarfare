d0cc6cbfea7ff28604dc7868ee9d53699a6f6f20
Miksuu <imiksuu96@gmail.com>
2023-08-01 23:48:34 +0300

    Fix default loadout tag for L159

--- Tools/LoadoutManager/Data/Aircrafts/Implementations/BLUFOR/L159ACR.cs
+++ Tools/LoadoutManager/Data/Aircrafts/Implementations/BLUFOR/L159ACR.cs
@@ -7,9 +7,9 @@
 
         base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
-                { AmmunitionType.THIRTYEIGHTROUNDHYDRA, 2},
-                { AmmunitionType.TWOROUNDMAVERICK, 2},
                 { AmmunitionType.TWOROUNDSIDEWINDER_AH1Z, 2},
+                { AmmunitionType.TWOROUNDMAVERICK, 2},
+                { AmmunitionType.THIRTYEIGHTROUNDHYDRA, 2},
             };
 
         allowedAmmunitionTypesWithTheirLimitationAmount = new Dictionary<AmmunitionType, int>
