1450eb63b798d0b23cfa6df5ab9e446b351a63ce
Miksuu <imiksuu96@gmail.com>
2023-08-02 04:09:20 +0300

    Move gbu comment on alca

--- Tools/LoadoutManager/Data/Aircrafts/Implementations/BLUFOR/L159ACR.cs
+++ Tools/LoadoutManager/Data/Aircrafts/Implementations/BLUFOR/L159ACR.cs
@@ -14,12 +14,12 @@
 
         allowedAmmunitionTypesWithTheirLimitationAmount = new Dictionary<AmmunitionType, int>
         {
-            //{ AmmunitionType.TWOROUNDGBU12, 2 },
             { AmmunitionType.TWOROUNDSIDEWINDER_AH1Z, 4 },
             { AmmunitionType.TWOROUNDMAVERICK, 0 },
             { AmmunitionType.THIRTYEIGHTROUNDHYDRA, 0 },
             { AmmunitionType.SIXROUNDMK82, 0 },
             { AmmunitionType.EIGHTROUNDHELLFIRE, 2 },
+            //{ AmmunitionType.TWOROUNDGBU12, 2 },
         };
 
         inGameDisplayName = "L-159";
