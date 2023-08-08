9373b1cacfda277053248c2b837e2a2ce2cdc77a
Miksuu <imiksuu96@gmail.com>
2023-08-08 16:35:17 +0300

    Generate the sqf code with GPT prompt to make copypasting easier

--- Tools/LoadoutManager/Program.cs
+++ Tools/LoadoutManager/Program.cs
@@ -5,6 +5,33 @@ class Program
 {
     static void Main()
     {
+        GenerateTheEasaFile();
+    }
+
+    private static void GenerateStartOfTheEasaFile()
+    {
+        Console.WriteLine("Private [\"_ammo\",\"_easaDefault\",\"_easaLoadout\",\"_easaVehi\",\"_is_AAMissile\",\"_loadout\",\"_loadout_line\",\"_vehicle\"];");
+        Console.WriteLine();
+        Console.WriteLine("EASA_Equip = Compile preprocessFileLineNumbers 'Client\\Module\\EASA\\EASA_Equip.sqf';");
+        Console.WriteLine("EASA_RemoveLoadout = Compile preprocessFileLineNumbers 'Client\\Module\\EASA\\EASA_RemoveLoadout.sqf';");
+        Console.WriteLine();
+        Console.WriteLine("_easaDefault = [];");
+        Console.WriteLine("_easaLoadout = [];");
+        Console.WriteLine("_easaVehi = [];");
+        Console.WriteLine();
+        Console.WriteLine("/* [[Price], [Description], [Weapon, Ammos], [Weapon, Ammos]...] */");
+    }
+
+
+    private static void GenerateTheEasaFile()
+    {
+        GenerateStartOfTheEasaFile();
+        GenerateAllAircraftLoadouts();
+        GenerateEndOfTheEasaFile();
+    }
+
+    private static void GenerateAllAircraftLoadouts()
+    {
         foreach (AircraftType aircraftType in Enum.GetValues(typeof(AircraftType)))
         {
             var _interfaceAircraft = (InterfaceAircraft)EnumExtensions.GetInstance(aircraftType.ToString());
@@ -13,4 +40,34 @@ class Program
             Console.WriteLine("\n");
         }
     }
+
+    private static void GenerateEndOfTheEasaFile()
+    {
+        Console.WriteLine("//--- Now we check for AA-based loadouts.");
+        Console.WriteLine("for '_i' from 0 to count(_easaVehi)-1 do {");
+        Console.WriteLine("\t_loadout = _easaLoadout select _i;");
+        Console.WriteLine("\t");
+        Console.WriteLine("\tfor '_j' from 0 to count(_loadout)-1 do {");
+        Console.WriteLine("\t\t_loadout_line = _loadout select _j;");
+        Console.WriteLine("\t\t_is_AAMissile = false;");
+        Console.WriteLine("\t\t");
+        Console.WriteLine("\t\t//--- Browse each EASA magazines and check the ammunition kind.");
+        Console.WriteLine("\t\t{");
+        Console.WriteLine("\t\t\t_ammo = getText(configFile >> \"CfgMagazines\" >> _x >> \"ammo\"); //--- We grab the ammo used by the magazine.");
+        Console.WriteLine("\t\t\t");
+        Console.WriteLine("\t\t\tif (_ammo != \"\") then {");
+        Console.WriteLine("\t\t\t\t//--- We check if the ammo is air-lock based and that in inherits from the missile class.");
+        Console.WriteLine("\t\t\t\tif (getNumber(configFile >> \"CfgAmmo\" >> _ammo >> \"airLock\") == 1 && configName(inheritsFrom(configFile >> \"CfgAmmo\" >> _ammo)) == \"MissileBase\") exitWith {_is_AAMissile = true};");
+        Console.WriteLine("\t\t\t};");
+        Console.WriteLine("\t\t} forEach ((_loadout_line select 2) select 1);");
+        Console.WriteLine("\t\t");
+        Console.WriteLine("\t\t_loadout_line set [3, if (_is_AAMissile) then {true} else {false}];");
+        Console.WriteLine("\t};");
+        Console.WriteLine("};");
+        Console.WriteLine();
+        Console.WriteLine("missionNamespace setVariable ['WFBE_EASA_Vehicles',_easaVehi];");
+        Console.WriteLine("missionNamespace setVariable ['WFBE_EASA_Loadouts',_easaLoadout];");
+        Console.WriteLine("missionNamespace setVariable ['WFBE_EASA_Default',_easaDefault];");
+    }
+
 }
