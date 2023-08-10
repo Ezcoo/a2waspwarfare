c25464b38ed8db54b8de53646d77d4ac801c58e9
Miksuu <imiksuu96@gmail.com>
2023-08-11 01:52:13 +0300

    Add av8b2

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/BLUFOR/AV8B_VARIANTS/AV8B2.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/BLUFOR/AV8B_VARIANTS/AV8B2.cs
@@ -5,8 +5,29 @@
         VehicleType = VehicleType.AV8B2;
         pylonAmount = 8;
 
+        //weapons[] = {
+        //    "GAU12",
+        //    "Mk82BombLauncher_6",
+        //    "SidewinderLaucher_AH1Z",
+        //    "FFARLauncher_14",
+        //    "CMFlareLauncher"
+        //};
+        //magazines[] = {
+        //    "300Rnd_25mm_GAU12",
+        //    "6Rnd_Mk82",
+        //    "2Rnd_Sidewinder_AH1Z",
+        //    "14Rnd_FFAR",
+        //    "120Rnd_CMFlare_Chaff_Magazine"
+        //};
+
+        base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+            {
+                { AmmunitionType.THREEHUNDREDROUNDGAU12, 2},
+                { AmmunitionType.VANILLA_SIXROUNDGBU12, 2},
+            };
         base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
+                { AmmunitionType.THREEHUNDREDROUNDGAU12, 2},
                 { AmmunitionType.TWOROUNDSIDEWINDER_AH1Z, 2},
                 { AmmunitionType.TWOROUNDMAVERICK, 6},
             };
