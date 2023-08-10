f67689442299de9954967f8b5abd405d132081a8
Miksuu <imiksuu96@gmail.com>
2023-08-10 16:52:57 +0300

    Add proper SU25a generation

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/SU25_VARIANTS/SU25A.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/SU25_VARIANTS/SU25A.cs
@@ -5,9 +5,31 @@
         VehicleType = VehicleType.SU25A;
         pylonAmount = 6;
 
+        //weapons[] = {
+        //    "GSh301",
+        //    "AirBombLauncher",
+        //    "R73Launcher_2",
+        //    "S8Launcher",
+        //    "CMFlareLauncher"
+        //};
+        //magazines[] = {
+        //    "180Rnd_30mm_GSh301",
+        //    "4Rnd_FAB_250",
+        //    "2Rnd_R73",
+        //    "80Rnd_S8T",
+        //    "120Rnd_CMFlare_Chaff_Magazine"
+        //};
+
+
+        base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+            {
+                { AmmunitionType.FOURROUNDFAB250, 2},
+                { AmmunitionType.TWOROUNDR73, 2},
+                { AmmunitionType.VANILLA_EIGHTYROUNDS8, 2},
+            };
         base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
-                { AmmunitionType.SIXROUNDFAB250, 4},
+                { AmmunitionType.FOURROUNDFAB250, 6},
                 { AmmunitionType.SIXTYFOURROUNDS5, 2},
             };
 
