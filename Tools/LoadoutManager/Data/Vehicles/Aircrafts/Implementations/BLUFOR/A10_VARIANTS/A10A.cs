b04f82133d7ba1987b9496928a252a83054fa37c
Miksuu <imiksuu96@gmail.com>
2023-08-11 15:09:41 +0300

    Add A10A

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/BLUFOR/A10_VARIANTS/A10A.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/BLUFOR/A10_VARIANTS/A10A.cs
@@ -5,6 +5,30 @@
         VehicleType = VehicleType.A10A;
         pylonAmount = 4;
 
+        //weapons[] = {
+        //    "GAU8",
+        //    "MaverickLauncher",
+        //    "SidewinderLaucher_AH1Z",
+        //    "BombLauncherA10",
+        //    "FFARLauncher_14",
+        //    "CMFlareLauncher"
+        //};
+        //magazines[] = {
+        //    "1350Rnd_30mmAP_A10",
+        //    "2Rnd_Maverick_A10",
+        //    "2Rnd_Sidewinder_AH1Z",
+        //    "4Rnd_GBU12",
+        //    "14Rnd_FFAR",
+        //    "120Rnd_CMFlare_Chaff_Magazine"
+        //};
+
+        base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+            {
+                { AmmunitionType.FOURTEENROUNDHYDRA, 2},
+                { AmmunitionType.FOURROUNDGBU12, 2},
+                { AmmunitionType.TWOROUNDSIDEWINDER_AH1Z, 2},
+                { AmmunitionType.TWOROUNDMAVERICK, 2},
+            };
         base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
                 { AmmunitionType.THIRTYEIGHTROUNDHYDRA, 2 },
