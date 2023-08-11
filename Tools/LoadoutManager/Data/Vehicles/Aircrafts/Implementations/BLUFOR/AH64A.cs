ae48ac0c625c7715555272ec846a2becc776be62
Miksuu <imiksuu96@gmail.com>
2023-08-11 15:21:25 +0300

    Add AH64A (TOW)

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/BLUFOR/AH64A.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/BLUFOR/AH64A.cs
@@ -5,10 +5,29 @@
         VehicleType = VehicleType.AH64A;
         pylonAmount = 2;
 
+        //weapons[] = {
+        //            "M230",
+        //            "SidewinderLaucher_AH64" ????
+        //        };
+        //magazines[] = {
+        //            "1200Rnd_30x113mm_M789_HEDP",
+        //            "8Rnd_Sidewinder_AH64" ????
+        //        };
+
         base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+            {
+                //{ AmmunitionType.SIXROUNDTOWTWO, 2},
+            };
+
+        base.vanillaGameDefaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+            {
+                { AmmunitionType.EIGHTROUNDHELLFIRE, 2},
+            };
+        base.defaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
                 { AmmunitionType.SIXROUNDTOWTWO, 2},
             };
+        turretPos = 0;
 
         allowedAmmunitionTypesWithTheirLimitationAmount = new Dictionary<AmmunitionType, int>
         {
