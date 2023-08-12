a86cab53bb20cf4cd3dfc5526604aa023cee6e38
Miksuu <imiksuu96@gmail.com>
2023-08-12 14:04:03 +0300

    Mi24p fixes

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/MI24P.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/MI24P.cs
@@ -18,13 +18,11 @@
 
         base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
-                { AmmunitionType.SEVENHUNDREDFIFTY30MMGSH301ROUNDS, 0},
-                { AmmunitionType.VANILLA_EIGHTYROUNDS8, 2},
+                { AmmunitionType.FOURROUNDATAKA, 4},
             };
         base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
-                { AmmunitionType.SEVENHUNDREDFIFTY30MMGSH301ROUNDS, 4},
-                { AmmunitionType.FOURTYROUNDS8, 2},
+                { AmmunitionType.FOURROUNDATAKA, 2},
             };
 
         //weapons[] = {
@@ -38,15 +36,16 @@
 
         base.vanillaGameDefaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
+                { AmmunitionType.SEVENHUNDREDFIFTY30MMGSH301ROUNDS, 0},
                 { AmmunitionType.TWOROUNDFAB250, 2},
-                { AmmunitionType.FOURROUNDATAKA, 4},
-
+                
+                { AmmunitionType.VANILLA_EIGHTYROUNDS8, 2},
             };
         base.defaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
-                { AmmunitionType.TWOROUNDFAB250, 2},
-                { AmmunitionType.FOURROUNDATAKA, 2},
-
+                { AmmunitionType.SEVENHUNDREDFIFTY30MMGSH301ROUNDS, 4},
+                { AmmunitionType.TWOROUNDFAB250, 0},
+                { AmmunitionType.FOURTYROUNDS8, 2},
             };
 
         allowedAmmunitionTypesWithTheirLimitationAmount = new Dictionary<AmmunitionType, int>
