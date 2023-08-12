977d5943d06d8d904c55779b2ed7598fcb98beba
Miksuu <imiksuu96@gmail.com>
2023-08-12 03:36:53 +0300

    Fix mi24d s5's not being on the turret

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/MI24DTKEP1.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/MI24DTKEP1.cs
@@ -5,14 +5,15 @@
         VehicleType = VehicleType.MI24DTKEP1;
         pylonAmount = 2;
 
-        base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+        base.vanillaGameDefaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
                 { AmmunitionType.VANILLA_ONEHUNDREDTWENTYEIGHTFOURROUNDS5, 2},
             };
-        base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+        base.defaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
                 { AmmunitionType.SIXTYFOURROUNDS5, 2},
             };
+        turretPos = -1;
 
         inGameDisplayName = "Mi-24D";
         inGameFactoryLevel = 3;
