35b2d9439a8d8b6d5edcb1619db6d38b1d335fe9
Miksuu <imiksuu96@gmail.com>
2023-08-12 03:12:37 +0300

    Fix su34

--- Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/SU34.cs
+++ Tools/LoadoutManager/Data/Vehicles/Aircrafts/Implementations/OPFOR/SU34.cs
@@ -6,27 +6,27 @@
         pylonAmount = 10;
 
         // Pilot seat
-        base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
-            {
-                { AmmunitionType.VANILLA_FOURTYROUNDS8, 2},
-            };
-        base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
-            {
-                { AmmunitionType.FOURTYROUNDS8, 2},
-            };
+        //base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+        //    {
+        //        { AmmunitionType.VANILLA_FOURTYROUNDS8, 2},
+        //    };
+        //base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+        //    {
+        //        { AmmunitionType.FOURTYROUNDS8, 2},
+        //    };
 
         // Turret
-        base.vanillaGameDefaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+        base.vanillaGameDefaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
                 { AmmunitionType.SIXROUNDCH29, 2},
                 { AmmunitionType.FOURROUNDR73, 2},
             };
-        base.defaultLoadoutOnTurret.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
+        base.defaultLoadout.AmmunitionTypesWithCount = new Dictionary<AmmunitionType, int>
             {
                 { AmmunitionType.SIXROUNDCH29, 2},
                 { AmmunitionType.TWOROUNDR73, 4},
             };
-        turretPos = -1;
+        //turretPos = -1;
 
         allowedAmmunitionTypesWithTheirLimitationAmount = new Dictionary<AmmunitionType, int>
         {
