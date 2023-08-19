1b3565453da491293d73aed43507c77395316582
Miksuu <imiksuu96@gmail.com>
2023-08-19 18:15:50 +0300

    Use recursive search for through all the directories to write the new files from Cherno/Takistan

--- Tools/LoadoutManager/Program.cs
+++ Tools/LoadoutManager/Program.cs
@@ -44,7 +44,6 @@ class Program
         return startOfTheEasaFile;
     }
 
-
     private static void GenerateCommonBalanceInitAndTheEasaFileForEachTerrain()
     {
         // Get the current executing directory
@@ -119,34 +118,61 @@ class Program
 
     private static void CopyFilesFromSourceToDestination(string _source, string _destination)
     {
-        // Get the list of files to copy
-        var sourceFiles = Directory.GetFiles(_source).Where(file =>
-            !file.EndsWith("mission.sqm", StringComparison.OrdinalIgnoreCase) &&
-            !file.EndsWith("version.sqf", StringComparison.OrdinalIgnoreCase)).ToList();
-
-        var destinationFiles = Directory.GetFiles(_destination);
+        // Ensure the destination directory exists
+        Directory.CreateDirectory(_destination);
 
         // Copy files from source to destination
-        foreach (var file in sourceFiles)
+        foreach (var file in Directory.GetFiles(_source))
         {
             string fileName = Path.GetFileName(file);
+
+            if (fileName.EndsWith("mission.sqm", StringComparison.OrdinalIgnoreCase) ||
+                fileName.EndsWith("version.sqf", StringComparison.OrdinalIgnoreCase))
+            {
+                continue;
+            }
+
             string destFile = Path.Combine(_destination, fileName);
             File.Copy(file, destFile, true);  // true to overwrite existing files
         }
 
+        // Recursively copy subdirectories
+        foreach (var directory in Directory.GetDirectories(_source))
+        {
+            string dirName = Path.GetFileName(directory);
+            if (dirName == null) continue;
+
+            string destDir = Path.Combine(_destination, dirName);
+            CopyFilesFromSourceToDestination(directory, destDir);
+        }
+
         // Delete extra files in destination that are not in source
-        foreach (var destFile in destinationFiles)
+        foreach (var destFile in Directory.GetFiles(_destination))
         {
             string fileName = Path.GetFileName(destFile);
+
             if (fileName.EndsWith("mission.sqm") || fileName.EndsWith("version.sqf"))
             {
                 continue;
             }
 
             string correspondingSourceFile = Path.Combine(_source, fileName);
-            if (!sourceFiles.Contains(correspondingSourceFile))
+            if (!File.Exists(correspondingSourceFile))
             {
                 File.Delete(destFile);
+            }
+        }
+
+        // Delete extra directories in destination that are not in source
+        foreach (var destDir in Directory.GetDirectories(_destination))
+        {
+            string dirName = Path.GetFileName(destDir);
+            if (dirName == null) continue;
+
+            string correspondingSourceDir = Path.Combine(_source, dirName);
+            if (!Directory.Exists(correspondingSourceDir))
+            {
+                Directory.Delete(destDir, true);  // true to remove all files and subdirectories
             }
         }
     }
