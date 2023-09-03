44d63adc11b4dcf804e46ab1193e7c6ca51f1e6f
Miksuu <imiksuu96@gmail.com>
2023-09-03 22:57:28 +0300

    Add EasaFileGenerator.cs

---
+++ Tools/LoadoutManager/FileManagement/EasaFileGenerator.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+
+public class EasaFileGenerator
+{
+    public static string GenerateStartOfTheEasaFile()
+    {
+        string startOfTheEasaFile = string.Empty;
+        startOfTheEasaFile += "Private [\"_ammo\",\"_easaDefault\",\"_easaLoadout\",\"_easaVehi\",\"_is_AAMissile\",\"_loadout\",\"_loadout_line\",\"_vehicle\"];";
+        startOfTheEasaFile += "\n";
+        startOfTheEasaFile += "EASA_Equip = Compile preprocessFileLineNumbers 'Client\\Module\\EASA\\EASA_Equip.sqf';";
+        startOfTheEasaFile += "EASA_RemoveLoadout = Compile preprocessFileLineNumbers 'Client\\Module\\EASA\\EASA_RemoveLoadout.sqf';";
+        startOfTheEasaFile += "\n";
+        startOfTheEasaFile += "\n_easaDefault = [];";
+        startOfTheEasaFile += "\n_easaLoadout = [];";
+        startOfTheEasaFile += "\n_easaVehi = [];";
+        startOfTheEasaFile += "\n\n";
+        startOfTheEasaFile += "/* [[Price], [Description], [Weapon, Ammos], [Weapon, Ammos]...] */";
+        return startOfTheEasaFile;
+    }
+
+    public static string GenerateEndOfTheEasaFile()
+    {
+        string endOfTheEasaFile = string.Empty;
+
+        endOfTheEasaFile += "for '_i' from 0 to count(_easaVehi)-1 do {";
+        endOfTheEasaFile += "\t_loadout = _easaLoadout select _i;";
+        endOfTheEasaFile += "\t";
+        endOfTheEasaFile += "\tfor '_j' from 0 to count(_loadout)-1 do {";
+        endOfTheEasaFile += "\t\t_loadout_line = _loadout select _j;";
+        endOfTheEasaFile += "\t\t_is_AAMissile = false;";
+        endOfTheEasaFile += "\t\t";
+        endOfTheEasaFile += "\t\t{";
+        endOfTheEasaFile += "\t\t\t_ammo = getText(configFile >> \"CfgMagazines\" >> _x >> \"ammo\");";
+        endOfTheEasaFile += "\t\t\t";
+        endOfTheEasaFile += "\t\t\tif (_ammo != \"\") then {";
+        endOfTheEasaFile += "\t\t\t\tif (getNumber(configFile >> \"CfgAmmo\" >> _ammo >> \"airLock\") == 1 && configName(inheritsFrom(configFile >> \"CfgAmmo\" >> _ammo)) == \"MissileBase\") exitWith {_is_AAMissile = true};";
+        endOfTheEasaFile += "\t\t\t};";
+        endOfTheEasaFile += "\t\t} forEach ((_loadout_line select 2) select 1);";
+        endOfTheEasaFile += "\t\t";
+        endOfTheEasaFile += "\t\t_loadout_line set [3, if (_is_AAMissile) then {true} else {false}];";
+        endOfTheEasaFile += "\t};";
+        endOfTheEasaFile += "};";
+        endOfTheEasaFile += "\n";
+        endOfTheEasaFile += "missionNamespace setVariable ['WFBE_EASA_Vehicles',_easaVehi];";
+        endOfTheEasaFile += "missionNamespace setVariable ['WFBE_EASA_Loadouts',_easaLoadout];";
+        endOfTheEasaFile += "missionNamespace setVariable ['WFBE_EASA_Default',_easaDefault];";
+
+        return endOfTheEasaFile;
+    }
+}
