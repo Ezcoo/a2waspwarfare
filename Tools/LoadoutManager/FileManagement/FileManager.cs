aadd8cf9b160980ed66d9885280d401c9880251e
Miksuu <imiksuu96@gmail.com>
2023-09-03 22:46:28 +0300

    Refactoring of the method

--- Tools/LoadoutManager/FileManagement/FileManager.cs
+++ Tools/LoadoutManager/FileManagement/FileManager.cs
@@ -1,27 +1,47 @@
-class FileManager
+using System;
+using System.IO;
+
+public class FileManager
 {
     public static void CopyFilesFromSourceToDestination(string _source, string _destination)
     {
-        // Ensure the destination directory exists
-        Directory.CreateDirectory(_destination);
+        EnsureDirectoryExists(_destination);
+        CopyFiles(_source, _destination);
+        RecursivelyCopySubdirectories(_source, _destination);
+        DeleteExtraFiles(_source, _destination);
+        DeleteExtraDirectories(_source, _destination);
+    }
 
-        // Copy files from source to destination
+    private static void EnsureDirectoryExists(string _directoryPath)
+    {
+        Directory.CreateDirectory(_directoryPath);
+    }
+
+    private static void CopyFiles(string _source, string _destination)
+    {
         foreach (var file in Directory.GetFiles(_source))
         {
             string fileName = Path.GetFileName(file);
 
-            if (fileName.EndsWith("mission.sqm", StringComparison.OrdinalIgnoreCase) ||
-                (fileName.EndsWith("version.sqf", StringComparison.OrdinalIgnoreCase) &&
-                !fileName.EndsWith("Init_Version.sqf", StringComparison.OrdinalIgnoreCase)))
+            if (ShouldSkipFile(fileName))
             {
                 continue;
             }
 
             string destFile = Path.Combine(_destination, fileName);
-            File.Copy(file, destFile, true);  // true to overwrite existing files
+            File.Copy(file, destFile, true);
         }
+    }
 
-        // Recursively copy subdirectories
+    private static bool ShouldSkipFile(string _fileName)
+    {
+        return _fileName.EndsWith("mission.sqm", StringComparison.OrdinalIgnoreCase) ||
+               (_fileName.EndsWith("version.sqf", StringComparison.OrdinalIgnoreCase) &&
+                !_fileName.EndsWith("Init_Version.sqf", StringComparison.OrdinalIgnoreCase));
+    }
+
+    private static void RecursivelyCopySubdirectories(string _source, string _destination)
+    {
         foreach (var directory in Directory.GetDirectories(_source))
         {
             string dirName = Path.GetFileName(directory);
@@ -30,15 +50,15 @@ class FileManager
             string destDir = Path.Combine(_destination, dirName);
             CopyFilesFromSourceToDestination(directory, destDir);
         }
+    }
 
-        // Delete extra files in destination that are not in source
+    private static void DeleteExtraFiles(string _source, string _destination)
+    {
         foreach (var destFile in Directory.GetFiles(_destination))
         {
             string fileName = Path.GetFileName(destFile);
 
-            if (fileName.EndsWith("mission.sqm") ||
-                (fileName.EndsWith("version.sqf", StringComparison.OrdinalIgnoreCase) &&
-                !fileName.EndsWith("Init_Version.sqf", StringComparison.OrdinalIgnoreCase)))
+            if (ShouldSkipFile(fileName))
             {
                 continue;
             }
@@ -49,8 +69,10 @@ class FileManager
                 File.Delete(destFile);
             }
         }
+    }
 
-        // Delete extra directories in destination that are not in source
+    private static void DeleteExtraDirectories(string _source, string _destination)
+    {
         foreach (var destDir in Directory.GetDirectories(_destination))
         {
             string dirName = Path.GetFileName(destDir);
@@ -59,7 +81,7 @@ class FileManager
             string correspondingSourceDir = Path.Combine(_source, dirName);
             if (!Directory.Exists(correspondingSourceDir))
             {
-                Directory.Delete(destDir, true);  // true to remove all files and subdirectories
+                Directory.Delete(destDir, true);
             }
         }
     }
