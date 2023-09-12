fca417bd5b07bdc039949e1db99772722229ebee
Miksuu <imiksuu96@gmail.com>
2023-09-12 11:53:12 +0300

    Add parameters to the version.sqf generation based on the terrain

--- Tools/LoadoutManager/Data/Terrains/BaseTerrain.cs
+++ Tools/LoadoutManager/Data/Terrains/BaseTerrain.cs
@@ -77,6 +77,12 @@ public abstract class BaseTerrain : InterfaceTerrain
         return TerrainType == TerrainType.FOREST ? "55" : "61";
     }
 
+    // Method to determine the mission camo based on the terrain type (Forest or Desert)
+    private int DetermineIfTheMissionHasCamoOn()
+    {
+        return TerrainType == TerrainType.FOREST ? 1 : 0;
+    }
+
     // Method to update files for modded terrains
     private void UpdateFilesForModdedTerrain(DirectoryInfo _dir)
     {
@@ -153,17 +159,24 @@ public abstract class BaseTerrain : InterfaceTerrain
         }
     }
 
-    public static void GenerateAndWriteVersionSqf(string _sqfFilePath)
+    public void GenerateAndWriteVersionSqf(string _sqfFilePath)
     {
+        int debug = 1;
+        int camo = DetermineIfTheMissionHasCamoOn();
+        string maxPlayers = DetermineMissionTypeIfItsForestOrDesert();
+        string missionName = @"[" + DetermineMissionTypeIfItsForestOrDesert() + @"] Warfare V48 " + EnumExtensions.GetEnumMemberAttrValue(terrainName.ToString());
+        int logContent = 1;
+
         // Define the SQF code specific to Chernarus terrain
-        string versionSqfCode = @"#define WF_DEBUG 1
-            #define WF_CAMO 
+        string versionSqfCode = 
+            $@"#define WF_DEBUG {debug}
+            #define WF_CAMO {camo}
             #define COMBINEDOPS 1
-            #define WF_MAXPLAYERS 55
-            #define WF_MISSIONNAME ""[55] Warfare V48 Chernarus""
+            #define WF_MAXPLAYERS {maxPlayers}
+            #define WF_MISSIONNAME ""{missionName}""
             #define WF_RESPAWNDELAY 2
             #define WF_LOADSCREEN __EVAL([""ca\Missions_e\campaign\missions\CE7B_PhoenixOp.Takistan\img\loading08_phoenixop_co.paa"",""ca\Missions_e\campaign\missions\CE7A_FinishingTouch.Takistan\img\loading08_finishingtouch_co.paa"",""ca\Missions_e\campaign\missions\CE6_EyeOfTheHurricane.Zargabad\img\loading07_co.paa"",""ca\Missions_e\campaign\missions\CE5B_FromHell.Takistan\img\loading06_fromhell_co.paa"",""ca\Missions_e\campaign\missions\CE5A_Sandstorm.Takistan\img\loading06_sandstorm_co.paa"",""ca\Missions_e\campaign\missions\CE4_OpenSeason.Takistan\img\loading05_co.paa"",""ca\Missions_e\campaign\missions\CE3_ColtanBlues.Takistan\img\loading04_co.paa"",""ca\Missions_e\campaign\missions\CE2_Pathfinder.Takistan\img\loading03_co.paa"",""ca\Missions_e\campaign\missions\CE1_GoodMorningTStan.Takistan\img\loading02_goodmorning_co.paa"",""ca\Missions_e\campaign\missions\CE0_Backstab.Zargabad\img\loading01_co.paa"",""ca\Missions_e\scenarios\SPE1_Jackal.Takistan\loading_jackal_co.paa"",""ca\Missions_e\scenarios\SPE1_Vehicles_US.Takistan\loading_showus_co.paa"",""ca\Missions_e\scenarios\SPE1_Vehicles_TKG.Zargabad\loading_showgue_co.paa"",""ca\Missions_e\scenarios\SPE1_Vehicles_TKA.Zargabad\loading_showtk_co.paa"",""ca\Missions_e\scenarios\SPE1_Vehicles_Civilian.Zargabad\loading_showciv_co.paa"",""ca\Missions_e\scenarios\SPE1_Vehicles_Allies.Takistan\loading_shownato_co.paa"",""ca\Missions_e\scenarios\SPE1_SteelPanthers.Takistan\loading_steelpanthers_co.paa"",""ca\Missions_e\scenarios\SPE1_OneShotOneKill.Takistan\loading_oneshotonekill_co.paa"",""ca\Missions_e\scenarios\SPE1_Littlebird.Takistan\loading_littlebird_co.paa"",""ca\Missions_e\scenarios\SPE1_LaserShow.Takistan\loading_lasershow_co.paa"",""ca\Missions_e\scenarios\SPE1_HikeInTheHills.Takistan\loading_hikeinthehills_co.paa"",""ca\Missions_e\scenarios\SPE1_DeathFromAbove.Takistan\loading_deathfromabove_co.paa"",""ca\Missions_e\scenarios\SPE1_Benchmark1.Takistan\loading_benchmark_co.paa"",""ca\Missions_e\MPScenarios\MPE1_Dogfighters.Takistan\loading_mpdogfight_co.paa"",""ca\Missions_e\MPScenarios\MPE_MountainWarfare.Takistan\loading_mpwarfare_co.paa"",""ca\Missions_e\MPScenarios\MPE_SectorControl.Zargabad\img\loading_mpsectorcontrol_co.paa""] select round random 25)
-            #define WF_LOG_CONTENT 1
+            #define WF_LOG_CONTENT {logContent}
             ";
 
         // Write the SQF code to the specified file
