554fbcdda0fd66d6db03a4e6cbf2ddef52bb4138
Miksuu <imiksuu96@gmail.com>
2023-09-12 10:14:13 +0300

    Minor syntax changes, reduce line length

--- Tools/LoadoutManager/SqfFileGenerators/SqfFileGenerator.cs
+++ Tools/LoadoutManager/SqfFileGenerators/SqfFileGenerator.cs
@@ -15,10 +15,13 @@ public class SqfFileGenerator
     public static string GenerateStartOfTheEasaFile()
     {
         string startOfTheEasaFile = string.Empty;
-        startOfTheEasaFile += "Private [\"_ammo\",\"_easaDefault\",\"_easaLoadout\",\"_easaVehi\",\"_is_AAMissile\",\"_loadout\",\"_loadout_line\",\"_vehicle\"];";
+        startOfTheEasaFile += "Private [\"_ammo\",\"_easaDefault\",\"_easaLoadout\",\"_easaVehi" +
+            "\",\"_is_AAMissile\",\"_loadout\",\"_loadout_line\",\"_vehicle\"];";
         startOfTheEasaFile += "\n";
-        startOfTheEasaFile += "EASA_Equip = Compile preprocessFileLineNumbers 'Client\\Module\\EASA\\EASA_Equip.sqf';";
-        startOfTheEasaFile += "EASA_RemoveLoadout = Compile preprocessFileLineNumbers 'Client\\Module\\EASA\\EASA_RemoveLoadout.sqf';";
+        startOfTheEasaFile += "EASA_Equip = Compile preprocessFileLineNumbers " +
+            "'Client\\Module\\EASA\\EASA_Equip.sqf';";
+        startOfTheEasaFile += "EASA_RemoveLoadout = Compile preprocessFileLineNumbers" +
+            " 'Client\\Module\\EASA\\EASA_RemoveLoadout.sqf';";
         startOfTheEasaFile += "\n";
         startOfTheEasaFile += "\n_easaDefault = [];";
         startOfTheEasaFile += "\n_easaLoadout = [];";
@@ -46,7 +49,9 @@ public class SqfFileGenerator
         endOfTheEasaFile += "\t\t\t_ammo = getText(configFile >> \"CfgMagazines\" >> _x >> \"ammo\");";
         endOfTheEasaFile += "\t\t\t";
         endOfTheEasaFile += "\t\t\tif (_ammo != \"\") then {";
-        endOfTheEasaFile += "\t\t\t\tif (getNumber(configFile >> \"CfgAmmo\" >> _ammo >> \"airLock\") == 1 && configName(inheritsFrom(configFile >> \"CfgAmmo\" >> _ammo)) == \"MissileBase\") exitWith {_is_AAMissile = true};";
+        endOfTheEasaFile += "\t\t\t\tif (getNumber(configFile >> \"CfgAmmo\" >> _ammo >> \"airLock\") == 1 &&" +
+            " configName(inheritsFrom(configFile >> \"CfgAmmo\" >> _ammo)) ==" +
+            " \"MissileBase\") exitWith {_is_AAMissile = true};";
         endOfTheEasaFile += "\t\t\t};";
         endOfTheEasaFile += "\t\t} forEach ((_loadout_line select 2) select 1);";
         endOfTheEasaFile += "\t\t";
@@ -107,7 +112,8 @@ public class SqfFileGenerator
     private static string GenerateCommonBalanceFileString()
     {
         string commonBalanceFileString = @"Private[""_currentFactoryLevel""];" + "\n\n";
-        commonBalanceFileString += "// After adding Pandur and BTR-90 to this script, it's necessary to exit on the server to prevent an occassional freeze\n";
+        commonBalanceFileString += "// After adding Pandur and BTR-90 to this script," +
+            " it's necessary to exit on the server to prevent an occassional freeze\n";
         commonBalanceFileString += "if (isServer) exitWith {};\n\n";
         commonBalanceFileString += "switch (typeOf _this) do\n{\n";
         commonBalanceFileString += commonBalanceInitFile;
@@ -140,7 +146,8 @@ public class SqfFileGenerator
 
     // WriteAndUpdateToFilesForTerrains takes in a DirectoryInfo object and two strings for EASA and common balance files.
     // It iterates through all defined terrains and writes or updates the respective files.
-    private static void WriteAndUpdateToFilesForTerrains(DirectoryInfo _dir, string _easaFileString, string _commonBalanceFileString)
+    private static void WriteAndUpdateToFilesForTerrains(
+        DirectoryInfo _dir, string _easaFileString, string _commonBalanceFileString)
     {
         foreach (var terrainName in Enum.GetValues(typeof(TerrainName)))
         {
