b1cb7b100abd327b65420db5aae085d19abd2818
Miksuu <imiksuu96@gmail.com>
2023-09-16 00:35:11 +0300

    Remove comment (blacklist removal finished)

--- Tools/LoadoutManager/FileManagement/FileManager.cs
+++ Tools/LoadoutManager/FileManagement/FileManager.cs
@@ -77,7 +77,6 @@ public class FileManager
             "Textures",
             @"Server\Config",
             @"Core_Artillery",
-            //@"Core_Units",
             @"Core_Structures",
         };
 
